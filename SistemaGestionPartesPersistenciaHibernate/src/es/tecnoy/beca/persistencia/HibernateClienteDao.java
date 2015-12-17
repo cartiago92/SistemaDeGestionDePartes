e84a4d3379b377ac18351045b3a3bb614f554131
Esther <esanzahijado@gmail.com>
2015-12-17 14:26:25 +0100

    Creados HibernateClienteDao e HibernateParteDao en el proyecto de PersistenciaHibernate

---
+++ SistemaGestionPartesPersistenciaHibernate/src/es/tecnoy/beca/persistencia/HibernateClienteDao.java
@@ -0,0 +1,50 @@
+package es.tecnoy.beca.persistencia;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import es.tecnoy.beca.entidades.Cliente;
+import es.tecnoy.beca.persistencia.interfaces.ClienteDao;
+
+public class HibernateClienteDao extends HibernateAbstractDao implements ClienteDao{
+
+	public HibernateClienteDao(SessionFactory sf) {
+		super();
+		this.setSf(sf);
+	}
+	
+	@Override
+	public void add(Cliente entidad) {
+		Session session = getSf().getCurrentSession();
+		Integer codigo = (Integer) session.save(entidad);
+		session.evict(entidad);
+		entidad.setCodigo(codigo);		
+	}
+
+	@Override
+	public void remove(Integer codigo) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Todavía no está implementado");
+	}
+
+	@Override
+	public void edit(Cliente entidad) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Todavía no está implementado");
+	}
+
+	@Override
+	public Cliente query(Integer codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> query() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ SistemaGestionPartesPersistenciaHibernate/src/es/tecnoy/beca/persistencia/HibernateParteDao.java
@@ -0,0 +1,95 @@
+package es.tecnoy.beca.persistencia;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import es.tecnoy.beca.entidades.Parte;
+import es.tecnoy.beca.entidades.Parte.EstadoParte;
+import es.tecnoy.beca.persistencia.interfaces.ParteDao;
+
+public class HibernateParteDao extends HibernateAbstractDao implements ParteDao {
+
+	public HibernateParteDao(SessionFactory sf) {
+		super();
+		this.setSf(sf);
+	}
+
+	@Override
+	public void add(Parte entidad) {
+		Session session = getSf().getCurrentSession();
+		Integer codigo = (Integer) session.save(entidad);
+		session.evict(entidad);
+		entidad.setCodigo(codigo);
+	}
+
+	@Override
+	public void remove(Integer codigo) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Todavía no está implementado");
+	}
+
+	@Override
+	public void edit(Parte entidad) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Todavía no está implementado");
+	}
+
+	@Override
+	public Parte query(Integer codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Parte> query() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Parte> queryBefore(Date fechaInicio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Parte> queryAfter(Date fechaInicio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// Consultar si no aprobado = rechazado o rechazado y pendiente
+	@Override
+	public void updateWhereEstadoNoAprobado(Parte entidad) {
+
+		Session session = getSf().getCurrentSession();
+
+		// StringBuilder hql = new StringBuilder("update Parte p set ")
+		// .append("p.descripcion = :descripcion, ")
+		// .append("p.fechaInicio = :fechaInicio, ")
+		// .append("p.fechaFin = :fechaFin, ")
+		// .append("p.estado = :estado, ")
+		// .append("p.motivoRechazo = :motivoRechazo ")
+		// .append("where p.codigo = :codigo and p.estado = :estadoPendiente");
+		//
+		// Query query = session.createQuery(hql.toString());
+
+		Query query = session.getNamedQuery("updateWhereEstadoNoAprobado");
+
+		query.setParameter("descripcion", entidad.getDescripcion());
+		query.setParameter("fechaInicio", entidad.getFechaInicio());
+		query.setParameter("fechaFin", entidad.getFechaFin());
+		query.setParameter("estado", entidad.getEstado());
+		query.setParameter("motivoRechazo", entidad.getMotivoRechazo());
+		query.setParameter("codigo", entidad.getCodigo());
+		query.setParameter("estadoPendiente", EstadoParte.PENDIENTE);
+
+		int registrosAfectados = query.executeUpdate();
+
+	}
+
+}
