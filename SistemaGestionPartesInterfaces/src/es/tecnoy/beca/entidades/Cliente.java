9960fb12df3fdb93f7c1151f6e574c49e230c112
Esther <esanzahijado@gmail.com>
2015-12-17 11:28:17 +0100

    Añadidas las entidades Cliente, Parte y Telefono

---
+++ SistemaGestionPartesInterfaces/src/es/tecnoy/beca/entidades/Cliente.java
@@ -0,0 +1,119 @@
+package es.tecnoy.modelo.entidad;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PTR_CLIENTES")
+public class Cliente {
+
+	/*
+	 * @Id
+	 * @GeneratedValue(strategy=GenerationType.IDENTITY) private Integer id;
+	 * @Column(unique=true) private String codigo;
+	 */
+
+	/*
+	 * @Id private String codigo;
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+
+	private String nombre;
+
+	@Column(columnDefinition = "text")
+	private String descripcion;
+	@Column(columnDefinition = "text")
+	private String observaciones;
+	private String web;
+
+	@Embedded
+	private Domicilio domicilio;
+
+	@OneToMany(mappedBy = "cliente", cascade = {CascadeType.ALL} /*, fetch=FetchType.EAGER */ )
+	private List<ContactoCliente> contactos;
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer codigo, String nombre, String descripcion, String observaciones, String web,
+			Domicilio domicilio) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.observaciones = observaciones;
+		this.web = web;
+		this.domicilio = domicilio;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public Domicilio getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(Domicilio domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public List<ContactoCliente> getContactos() {
+		return contactos;
+	}
+
+	public void setContactos(List<ContactoCliente> contactos) {
+		this.contactos = contactos;
+	}
+
+}
