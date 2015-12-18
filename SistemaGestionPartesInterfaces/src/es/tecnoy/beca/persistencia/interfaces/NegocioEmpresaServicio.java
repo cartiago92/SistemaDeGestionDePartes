9b3ab751c7c663adf70032fef68f2bf044dc052e
Angel <angel_gv93@hotmail.com>
2015-12-18 13:20:45 +0100

    Añadidas interfaz NegocioEmpresaServicio y NegocioTecnico

---
+++ SistemaGestionPartesInterfaces/src/es/tecnoy/beca/persistencia/interfaces/NegocioEmpresaServicio.java
@@ -0,0 +1,10 @@
+package es.tecnoy.beca.persistencia.interfaces;
+
+import es.tecnoy.beca.entidades.EmpresaServicio;
+
+public interface NegocioEmpresaServicio {
+
+	void altaEmpresa (EmpresaServicio empresa);
+	
+}
+
---
+++ SistemaGestionPartesInterfaces/src/es/tecnoy/beca/persistencia/interfaces/NegocioTecnico.java
@@ -0,0 +1,9 @@
+package es.tecnoy.beca.persistencia.interfaces;
+
+import es.tecnoy.beca.entidades.Tecnico;
+
+public interface NegocioTecnico {
+
+		void altaTecnico(Tecnico tecnico);
+
+}
